d57f8db3a188548117232ecf592a7f1ce1c0c182
dsemenkova <diana.semenkova.00@mail.ru>
2023-05-06 20:05:25 +0300

    апрол

---
+++ ServiceCenter.View/Areas/Guest/Controllers/GuestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceCenter.View.Areas.Guest.Controllers
+{
+    public class GuestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
