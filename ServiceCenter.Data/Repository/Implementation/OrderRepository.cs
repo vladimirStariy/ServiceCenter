d5c482ce32f9cf634d66fdda00bda37883bc04d7
vladimirStary <puffychords@gmail.com>
2023-05-07 03:00:53 +0300

    quick fix

--- ServiceCenter.Data/Repository/Implementation/OrderRepository.cs
+++ ServiceCenter.Data/Repository/Implementation/OrderRepository.cs
@@ -1,10 +1,4 @@
-﻿ing System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServiceCenter.Data.Repository.Implementation
+﻿namespace ServiceCenter.Data.Repository.Implementation
 {
     internal class OrderRepository
     {
